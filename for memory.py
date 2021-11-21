08cee2f38b84df4d97c672d7299927dcb0377a5d
NameAlex_Spo <spoleducacion@gmail.com>
2021-11-21 17:49:41 +0100

    Initial commit

---
+++ for memory.py
@@ -0,0 +1,16 @@
+L = list(map(float, input().split()))
+print(L)
+h = float, input().split()
+print(h)
+
+L = (1, 3, 5, 8)
+a = min(L)
+print(a)
+
+L = [1, 3, 5, 8]
+a = min(L)
+print(a)
+
+L = [1, 3, 5, 8]
+a = L[1:]
+print(a)
