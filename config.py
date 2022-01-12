49fe628c17853abc85e2d14e240f06eb81d86e4e
Alex-Spo <94808146+Alex-Spo@users.noreply.github.com>
2022-01-12 23:25:59 +0100

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "5040425227:AAEo3PkZaouJKw9OUHQfBJMIeIKA2WU4wN0"
-keys = {'биткоин' : 'BTC',
-        'эфириум' : 'ETH',
-        'риппл' : 'XRP',
-        'лайткоин' : 'LTC',
-        'люмен' : 'XLM',
-        'доллар' : 'USD',
-        'евро' : 'EUR',
-        }
